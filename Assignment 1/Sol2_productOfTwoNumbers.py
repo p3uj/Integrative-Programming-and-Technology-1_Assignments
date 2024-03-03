86fb39a2662168a0b3ef4ba64c570316fec368cc
Bengie <bvillesco@gmail.com>
2024-03-03 14:54:35 +0800

    Assignment 1: Add Sol2_productOfTwoNumbers.py

---
+++ Assignment 1/Sol2_productOfTwoNumbers.py
@@ -0,0 +1,9 @@
+# User input
+number1 = eval(input("Enter first number: ")) # Ask user to input the first number and evaluate it (i.e int or float data type).
+number2 = eval(input("Enter second number: ")) # Ask user to input the second number and evaluate it (i.e int or float data type).
+
+# Calculate the product of two numbers.
+productOfTwoNumbers = number1 * number2
+
+# Display the product of two numbers.
+print(f"The product of {number1} and {number2} is {productOfTwoNumbers}")
