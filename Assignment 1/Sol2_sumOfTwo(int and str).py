4f00f1dc1e81796f30a6533e2d95056cf9404ce5
Bengie <bvillesco@gmail.com>
2024-03-03 15:33:40 +0800

    Assignment 1: Sol2_sumOfTwo(int and str).py

---
+++ Assignment 1/Sol2_sumOfTwo(int and str).py
@@ -0,0 +1,12 @@
+# Initialize and Assign value to variables
+x = 3
+ys = '24'
+
+# Convert the data type of ys(i.e str) to int using typecasting.
+ys = int(ys)
+
+# Calcucate the sum of x and ys
+sumOfTwo = x + ys
+
+# Display the sum of two.
+print(f"The sum of {x} and {ys} is {sumOfTwo}")
