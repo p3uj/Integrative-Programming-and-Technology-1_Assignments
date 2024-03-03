488e0b33aff6f5cb42bed0d70701be1463fbf050
Bengie <bvillesco@gmail.com>
2024-03-03 15:16:08 +0800

    Assignment 1/Sol1_sumOfTwo(int and str).py

---
+++ Assignment 1/Sol1_sumOfTwo(int and str).py
@@ -0,0 +1,3 @@
+x = 3 # Assign the value of x which is 3.
+ys = '24' # Assign the value of ys which is '24'.
+print(f"The sum of {x} and {ys} is {x + int(ys)}") # Convert the data type of ys(i.e str) to int. Then calculate the sum and display it.
