e710160eb11d0def91e93dee9956a2fe01dcc8a0
Bengie <bvillesco@gmail.com>
2024-03-28 13:33:58 +0800

    Modified: Add codes for this file.

---
+++ Assignment 3/creating_a_list_using_append_and_concatenation.py
@@ -0,0 +1,16 @@
+def list_of_items():
+    items = []
+
+    # Add the first four items to the list called items using append. (one per item).
+    items.append(67)
+    items.append(62.9)
+    items.append("hi")
+    items.append(False)
+
+    # Add the last four items to the list called items using concatinate.
+    items += [8, 67, 'apple', 6.5]
+
+    return items
+
+# Function call to list_of_items function then display the return list of items.
+print(list_of_items(), end='')  # The end='' is used to specify that there is no newline.
