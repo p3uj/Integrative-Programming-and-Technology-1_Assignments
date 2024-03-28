28a8385ace2f36bf89e91c439dfb46d26e7dacda
Bengie <bvillesco@gmail.com>
2024-03-28 15:34:17 +0800

    Add list transformation.py

---
+++ Assignment 3/list transformation.py
@@ -0,0 +1,8 @@
+items = [8, 9, 10]  # Initial value of the list.
+items[1] = 17   # Set the second entry (index 1) to 17.
+items.extend([4, 5, 6]) # Add 4, 5 and 6 to the end of the list.
+items.pop(0)    # Remove the first entry from the list.
+items.sort()    # Sort the list
+items = 2 * items   # Double the elements of the list.
+items.insert(3, 25) # Insert 25 at index 3
+print(items)    # Print the final elements of list.
