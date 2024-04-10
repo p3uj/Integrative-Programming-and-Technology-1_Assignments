0bc2843b627605d41c03c3394787560dc47cf035
Bengie <bvillesco@gmail.com>
2024-04-10 11:30:07 +0800

    Modified month_day_dictionary.py : Print out the (key-value) pairs sorted by the number of days in each month.

--- Assignment 4/month_day_dictionary.py
+++ Assignment 4/month_day_dictionary.py
@@ -7,14 +7,14 @@ c)	Print out all of the months with 31 days.
 d)	Print out the (key-value) pairs sorted by the number of days in each month
 
 """
-
-days = {'January':31, 'February':28,
-        'March':31, 'April':30,
-        'May':31, 'June':30,
-        'July':31, 'August':31,
-        'September':30, 'October':31,
-        'November':30, 'December':31
-        }
+days = {
+    'January':31, 'February':28,
+    'March':31, 'April':30,
+    'May':31, 'June':30,
+    'July':31, 'August':31,
+    'September':30, 'October':31,
+    'November':30, 'December':31
+}
 
 # Print all the month's name
 print("─" * 100 + "\n" + "MONTH'S NAME".center(100) + "\n" + "─" * 100)
@@ -29,11 +29,10 @@ Alternative solution to print all the month's name
 print(days.keys())
 """
 
-
-month = input("Enter a month name: ").title()   # Ask user to input a month name.
+month = input("Enter a month name: ").title()   # Ask user to input a month name then convert every first letter of the word into uppercase.
 while month not in days:    # Loop as long as the user's inputted for the month is not in the dictionary.
     print(f"\n'{month}' is not in the dictionary! Please enter valid month!")
-    month = input("Enter a month name: ").title()
+    month = input("Enter a month name: ").title() # Ask user to input a month name then convert every first letter of the word into uppercase.
 print(f"There are {days[month]} days in a month of {month}!\n")   # Tell user how many days are in the month.
 print(f"Keys in alphabetical order:\n{sorted(days)}\n") # Sort the keys in alphabetical order then print all the keys.
 
@@ -42,5 +41,8 @@ for key in days:
     if days[key] == 31:
         print(key)
 
-
+# Sort all items in the days dict according to the days in months.
+sorted_dict = dict(sorted(days.items(), key=lambda x: x[1]))
 print("\nMONTHS THAT ARE SORTED BY NUMBER OF DAYS:")
+for key in sorted_dict:
+    print(f"{key}: {sorted_dict[key]}")
