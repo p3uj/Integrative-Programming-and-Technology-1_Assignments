cb812e88ecd4fae424227eb7a4b6fa5db65492ec
Bengie <bvillesco@gmail.com>
2024-05-25 16:54:06 +0800

    Create GUI folder and add TkinterFirstProgram.py

---
+++ GUI/TkinterFirstProgram.py
@@ -0,0 +1,12 @@
+#import tkinter module for GUI programming
+import tkinter
+
+#create the main/parent window name root
+root = tkinter.Tk()
+
+#assigning title to the Window
+root.title("My Window")
+
+#tell the main window to run in an infinite loop, until it is
+#explicitly terminated.
+root.mainloop()
